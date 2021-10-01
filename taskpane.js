660aca7ef079b533f8da4ff4845730bc2715fec3
Alan Le Marquand <Alan.LeMarquand@exclaimer.com>
2021-10-01 14:57:51 +0100

    Token test for task pane

--- taskpane.js
+++ taskpane.js
@@ -3,6 +3,8 @@ Office.initialize = function () {
 };
 
 
-Office.onReady(info =>{
+Office.onReady(async info =>{
     document.getElementById('address').innerHTML = `<p>${Office.context.mailbox.userProfile.emailAddress}`;
+    document.getElementById('token').innerHTML = `<p>${await OfficeRuntime.auth.getAccessToken({ allowConsentPrompt: false, allowSignInPrompt: false })}`;
+    
 })
